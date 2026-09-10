4031d1dac78c4b88540333ff32bdb65f636ad92b
Liam Pham <Liamp@LIAMPHAM>
2026-09-10 10:55:45 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int column = 0; column < size; column++)
+                {
+                    if ((row + column) % 2 == 0)
+                    {
+                        Console.Write("□");
+                    }
+                    else
+                    {
+                        Console.Write("■");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
